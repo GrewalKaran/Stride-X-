37a7d98509126e80f7d538a09c8e984ba67b5ecf
Karan grewal <karangrewal979@gmail.com>
2026-05-19 12:48:34 +0530

    add map to run report

--- frontend/src/features/run/pages/RaceReport.jsx
+++ frontend/src/features/run/pages/RaceReport.jsx
@@ -4,6 +4,7 @@ import { raceReport } from "../services/run.api";
 import { useAuth } from "@clerk/clerk-react";
 
 import { RaceContext } from "../../../app.context";
+import RaceMap from "../components/RaceMap";
 
 const RaceReport = () => {
   const { loading, setLoading, finalRaceData, setFinalRaceData } =
@@ -41,6 +42,11 @@ const RaceReport = () => {
     return <h1>Loading...</h1>;
   }
 
+  const route = finalRaceData?.route || [];
+
+  const position =
+    route.length > 0 ? [route[0].lat, route[0].lng] : [28.6139, 77.209];
+
   return (
     <div>
       <h1>Race Report</h1>
@@ -48,6 +54,8 @@ const RaceReport = () => {
       <p>Distance: {finalRaceData?.distance}</p>
 
       <p>Duration: {finalRaceData?.duration}</p>
+
+      {route.length > 0 && <RaceMap position={position} route={route} />}
     </div>
   );
 };
