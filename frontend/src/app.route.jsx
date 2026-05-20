002ec90745a6cb483bbbca3a1e7b5b7b77e28a54
Karan grewal <karangrewal979@gmail.com>
2026-05-20 17:06:57 +0530

    fix typo

--- frontend/src/app.route.jsx
+++ frontend/src/app.route.jsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from 'react-router'
 import Register from './features/auth/pages/Register';
-import Login from './features/auth/pages/login';
+import Login from './features/auth/pages/Login';
 import Home from './features/run/pages/Home';
 import SoloRace from './features/run/pages/SoloRace';
 import RaceReport from './features/run/pages/RaceReport';
